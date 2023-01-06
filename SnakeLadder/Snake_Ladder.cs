be9581d7cd9d7f1944532dcc2c35f0b5104c2bb4
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2023-01-06 17:53:20 +0530

    [Nehal]Adding: Roll the die until the player reaches 100

--- SnakeLadder/Snake_Ladder.cs
+++ SnakeLadder/Snake_Ladder.cs
@@ -9,24 +9,28 @@ namespace SnakeLadder
         public static void dice()
         {
             int p = 0;
-            Random obj= new Random();
-            int check=obj.Next(0,7);
-            Console.WriteLine("you have got {0} ",check);
-            
-            int a=obj.Next(1,3);
-            if (check == 0)
+            while (p != 100)
             {
-                Console.WriteLine("You have to stay");
-            }
-           else if (a==1)
-            {
-                p += check;
-                Console.WriteLine("Congratulation, Step the ladder by getting"+ p);
-            }
-            else
-            {
-                p -= check;
-                Console.WriteLine("Sorry, it's snake step dowen by getting"+ p);
+                Random obj = new Random();
+                int check = obj.Next(0, 7);
+                Console.WriteLine("you have got {0} ", check);
+
+                int a = obj.Next(1, 3);
+                if (check == 0)
+                {
+                    Console.WriteLine("You have to stay");
+                }
+                else if (a == 1)
+                {
+                    p += check;
+                    Console.WriteLine("Congratulation, Step the ladder by getting" + p);
+                    p++;
+                }
+                else
+                {
+                    p -= check;
+                    Console.WriteLine("Sorry, it's snake step dowen by getting" + p);
+                }
             }
              
         }
