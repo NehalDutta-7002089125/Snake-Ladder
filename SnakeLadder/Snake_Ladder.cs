97d38e81601c6f21bb0c3f79b1bc2941e52c36aa
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2023-01-06 16:31:46 +0530

    [Nehal]Adding: Sinle player at start 0

---
+++ SnakeLadder/Snake_Ladder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    public class Snake_Ladder
+    {
+        public static void dice()
+        {
+            Console.WriteLine("Roll the dice");
+            Random obj= new Random();
+            int check=obj.Next(0,7);
+            if(check==0)
+            {
+                Console.WriteLine("You are in start");
+            }
+            else
+            {
+                Console.WriteLine("You have moved to {0} postition", check);
+            }
+        }
+    }
+}
